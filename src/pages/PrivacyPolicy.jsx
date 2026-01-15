7206b7b50dc3b00b8dfa992f091d73901d93fdd4
Arijit Saikia <your-email@example.com>
2026-01-15 16:15:56 +0530

    Debug: isolate PrivacyPolicy render

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "red",
+        color: "white",
+        fontSize: "40px",
+        padding: "40px",
+      }}
+    >
+      🚫 THIS IS PRIVACY POLICY ONLY
+    </div>
+  );
+}
+
+  
